1808060dae43bdf38360eb2c1c5f38ecdfc681e1
unknown <tpgus1196@naver.com>
2022-02-11 08:43:20 +0900

    mutation 데이터 추출

--- server.js
+++ server.js
@@ -15,30 +15,33 @@ const typeDefs = gql`
     }
     type Query{
         movies: [Movie]
-        movie: Movie
+        movie(id:Int!): Movie
     }
     type Mutation{
-        createMovie(title:String!):Boolean
-        deleteMovie(title:String!):Boolean
+        createMovie(title:String! year:Int! genre:String):Movie
+        deleteMovie(id:Int!):Movie
+        updateMovie(id:Int! year:Int!):Movie
     }
 `;
-
+//playgroun에서 mutation데이터 뽑는 법
 const resolvers = {
     Query: {
         movies: () => client.movie.findMany(),
-        movie: () => ({"title":"Hello", year: 2022}), //"title", year의 차이
+        movie: (_,{id}) => client.movie.findUnique({where:{id}}), //"title", year의 차이
     },
     Mutation: {
         createMovie: (_,{title,year,genre}) => 
-        {
-            console.log({title});
-            return true;
-        },
-        deleteMovie: (_,{title}) => 
-       { 
-            console.log({title});
-            return true;
-        }, 
+        client.movie.create(
+            {
+                data:{
+                    title,
+                    year,
+                    genre,
+                },
+            }
+        ),
+        deleteMovie: (_,{id}) => client.movie.delete({where:{id}}), 
+        updateMovie: (_,{id, year}) => client.movie.update({where:{id}, data:{year}})
     },
     
 };
